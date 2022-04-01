e57d80fdbf4c055577af33929c134a34274273b9
Hellgrimm <minecrafttima13@gmail.com>
2022-04-02 00:25:12 +0300

    Alpha v1.4.1 Why it is not working

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,19 +15,21 @@ export default function Navbar() {
   const {title} = data.site.siteMetadata
   
   return (
-    <nav className="navbar navbar-dark navbar-expand-lg">
-      <div className="container-fluid">
-        <h1>{title}</h1>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="links collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
-            <div className="link"><Link to="/">Home</Link></div>
-            
-            <div className="link"><Link to="/characters">Characters</Link></div>
-            <div className="link"><Link to="/locations">Locations</Link></div>
-            <div className="link"><Link to="/about">About</Link></div>
+    <nav>
+      <div className="navbar navbar-dark navbar-expand-lg">
+        <div className="container-fluid">
+          <h1>{title}</h1>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="links collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <div className="link"><Link to="/">Home</Link></div>
+              
+              <div className="link"><Link to="/characters">Characters</Link></div>
+              <div className="link"><Link to="/locations">Locations</Link></div>
+              <div className="link"><Link to="/about">About</Link></div>
+            </div>
           </div>
         </div>
       </div>
