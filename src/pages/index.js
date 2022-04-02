3837fa8ed10df44c71896f1f8a806c7494548e2c
Hellgrimm <minecrafttima13@gmail.com>
2022-04-02 13:34:48 +0300

    Alpha v1.5 Optimisation

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,22 +2,28 @@ import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
 import * as styles from '../styles/home.module.css'
-import '../styles/global.css'
 import Img from "gatsby-image"
 
 export default function Home({ data }) {
   return (
     <Layout>
     <section className={styles.header}>
-      <div>
-      <h2>Main Page</h2>
-      <h3>Hi, my name is Helly and this is a new world that i create.</h3>
-      <p>What do you think? Thats a first page of it, just testing things.</p>
-      <p>Thats another paragraph. Nice to see you!</p>
-      <Link className={styles.btn} to="/characters">To Characters Page</Link>
+      <div className={styles.head}>
+        <h2>Main Page</h2>
+        <h3>Welcome to the site of Game! </h3>
+        <p>There we would place information about characters, locatins, our plans and other. U can check the progress of development here:</p>
+        <div className={styles.button}>
+          <Link className={styles.btn} to="/about">Development Page</Link>
+        </div>
       </div>
       <div className={styles.pic}>
         <Img fluid={data.file.childImageSharp.fluid}/>
+      </div>
+    </section>
+    <section className={styles.main}>
+      <div className={styles.firstpar}>
+        <h3>First Paragraph</h3>
+        <p>Thats another paragraph. Nice to see you!</p>
       </div>
     </section>
     </Layout>
